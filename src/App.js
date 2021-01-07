4c0c0ab472e16b3416810d019bad271774803b24
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-07 16:10:26 +0200

    useEffect mousemove listener

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, {useState, useEffect} from 'react';
 function App() {
   const [type, setType] = useState('users');
   const [data, setData] = useState('data');
+  const [pos, setPos] = useState({
+    x:0, y: 0
+  })
   // useEffect(() => {
   //   console.log('render');
   // })
@@ -13,10 +16,24 @@ function App() {
       .then(json => setData(json))
   }, [type]); //rerender if type was changed
 
+  const mouseMoveHandler = event => {
+    setPos({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
   useEffect(() => {
     console.log('ComponentDidMount')
-  }, [])
+
+    window.addEventListener('mousemove', mouseMoveHandler)
+    
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    }
   
+  }, [])
+
   return (
     <div>
       <h1>Resource: {type}</h1>
@@ -25,7 +42,8 @@ function App() {
       <button class="btn btn-warning" onClick={()=> setType('todos')}>Todos</button>
       <button class="btn btn-info" onClick={()=> setType('posts')}>Posts</button>
 
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
 
     </div>
   );
