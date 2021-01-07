21d78f4088914d7e653d760586a36dcb69d94dd1
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-07 17:14:49 +0200

    useMemo problem with delay in wrong call function - colored

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, {useState, useMemo} from 'react';
 
 function complexCompute(num){
+  console.log('complexCompute')
   let i = 0
-  while (i < 1000000000) i++
+  while (i < 1000000000) i++ //for delay
   return num * 2
 }
 
 function App() {
   const [number, setNumber] = useState(42)
-
+  const [colored, setColored] = useState(false)
   const computed = complexCompute(number)
+
+  const styles = {
+    color: colored ? 'gold' : 'green'
+  }
 
   return (
     <div>
-      <h1>Computed number: {number}</h1>
+      <h1 style={styles}>Computed number: {number}</h1>
       <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Add</button>
       <button className="btn btn-danger" onClick={() => setNumber(prev => prev - 1)}>Delete</button>
+      <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Change</button>
     </div>
   );
 }
