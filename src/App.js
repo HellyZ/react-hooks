f5759046c50f1f8b5ae46302fa957399ba09e3dc
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-07 16:55:05 +0200

    useRef use it for binding DOM element and focus on

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, {useState, useEffect, useRef} from 'react';
 
+
 function App() {
-  const [renderCount, setRenderCount] = useState(1);
+  // const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState('initial')
+  const renderCount = useRef(1) // object with .current
+  const inputRef = useRef(null) //binding to DOM element input
 
   useEffect(() => {
-    setRenderCount(prev => prev + 1) //зацикливание
+    renderCount.current++ //didnt rerendered
+    console.log(inputRef.current.value);
   })
+
+  const focus = () => inputRef.current.focus()
 
   return (
     <div>
-    <h1>Amount of renders: {renderCount}</h1>
+      <div className="container">
+      <h1>Amount of renders: {renderCount.current}</h1>
+        <div className="row justify-content-start">
+          <div className="col-6">
+            <input ref={inputRef} type="text" onChange={(e) => setValue(e.target.value)} value={value} />
+            <button className="btn btn-success" onClick={focus}>Focus</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
