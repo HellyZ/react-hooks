c19e7442c7dd0181f4e4415d32d45c0a08c6e021
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-07 15:18:15 +0200

    useState for counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // для классовых компонентов this.setState({a:1}) -> this.render()
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
+  function decrement(){
+    setCounter(counter - 1);
+  }
+
   return (
     <div>
-     <h1>Counter</h1>
-     <button className="btn btn-primary">Add</button>
-     <button className="btn btn-danger">Delete</button>
+     <h1>Counter: {counter}</h1>
+     <button onClick={increment} className="btn btn-primary">Add</button>
+     <button onClick={decrement} className="btn btn-danger">Delete</button>
     </div>
   );
 }
