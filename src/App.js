89bc5a4a7d794cba9db05b005d364033fb079adf
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-07 21:27:01 +0200

    useContext sceleton for new hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { useState, useCallback} from 'react';
-import ItemList from './itemsList';
+import Main from './Main';
 
 
 function App() {
-  const [colored, setColored] = useState(false);
-  const [count, setCount] = useState(1)
-
-  const styles = {
-    color: colored ? 'gold' : 'green'
-  }
-
-  const generateItemsFromAPI = useCallback(() => {
-    return new Array(count).fill('').map((_, i) => `Element ${i+1}`) //fake server call
-  }, [count])
-
+ 
   return (
-    <div>
-      <h1 style={styles}>Elements amount: {count}</h1>
-      <button className="btn btn-success" onClick={() => setCount(prev => prev + 1)}>Add</button>
-      <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Change</button>
-      <ItemList getItems={generateItemsFromAPI} />
+    <div className="container">
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <>
+      <h1>  Hello! It's example with useContext</h1>
+      <button className="btn btn-success">Show alert</button>
+    </>
+  )
+}
