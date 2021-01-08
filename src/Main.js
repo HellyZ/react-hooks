41c8aa3bcce96854c744f80c46df390b83ccf70b
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-08 22:00:29 +0200

    useContext simple props provider via useContext

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Main() {
+export default function Main({ toggle }) {
   return (
     <>
       <h1>  Hello! It's example with useContext</h1>
-      <button className="btn btn-success">Show alert</button>
+      <button onClick={toggle} className="btn btn-success">Show alert</button>
     </>
   )
 }
