91f5aea939ce27656a4a83966028bc537669eb95
Alena Zhmurko <alenazhmurko@gmail.com>
2021-01-09 00:24:00 +0200

    useReducer alert change state

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useAlert } from './alert/AlertContext';
 
 export default function Main() {
-  const {toggle} = useAlert();
+  const {show} = useAlert();
   return (
     <>
       <h1>  Hello! It's example with useContext</h1>
-      <button onClick={toggle} className="btn btn-success">Show alert</button>
+      <button onClick={show} className="btn btn-success">Show alert</button>
     </>
   )
 }
--- src/alert/AlertContext.js
+++ src/alert/AlertContext.js
@@ -1,21 +1,35 @@
-import React, { useContext, useState } from 'react';
-
+import React, { useContext, useReducer } from 'react';
 
 const AlertContext = React.createContext()
 
 export const useAlert = () => {
-  return useContext(AlertContext)
+  return useContext(AlertContext);
+}
+
+const SHOW_ALERT = 'show';
+const HIDE_ALERT = 'hide';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SHOW_ALERT : return {...state, visible: true}
+    case HIDE_ALERT : return {...state, visible: false}
+    default: return state
+  }
 }
 
 export const AlertProvider = ({ children }) => {
-  const [alert, setAlert] = useState(false);
 
-  const toggle = () => setAlert(prev => !prev);
+  const [state, dispatch] = useReducer(reducer, {
+    visible: false
+  })
+
+  const show = () => dispatch({ type: SHOW_ALERT })
+  const hide = () => dispatch({ type: HIDE_ALERT })
 
   return (
     <AlertContext.Provider value={{
-      visible: alert,
-      toggle
+      visible: state.visible,
+      show, hide
     }}>
       { children }
     </AlertContext.Provider>
